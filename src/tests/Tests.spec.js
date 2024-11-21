3b4bf5140ba02a71d640c1fd7669146a588676eb
Rafael da Silva Castro <rafaeldasilvacastroti@gmail.com>
2024-11-21 20:20:45 -0300

    salvando api

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,17 +1,20 @@
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 import { Trend } from 'k6/metrics';
 
 export const getContactsDuration = new Trend('get_contacts', true);
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.01'],
+    http_req_failed: ['rate<0.90'],
     http_req_duration: ['avg<10000']
   },
-  stages: [{ duration: '20s', target: 20 }]
+  stages: [
+    { duration: '60s', target: 10 },
+    { duration: '60s', target: 10 }
+  ]
 };
 
 export function handleSummary(data) {
@@ -22,7 +25,7 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const baseUrl = 'https://api.thecatapi.com/v1/images/search' //'https://viacep.com.br/ws/88816820/json/';
 
   const params = {
     headers: {
@@ -32,11 +35,11 @@ export default function () {
 
   const OK = 200;
 
-  const res = http.get(`${baseUrl}`, params);
+  const res = http.get(`${baseUrl}`);
 
   getContactsDuration.add(res.timings.duration);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'get contacts - status 200': () => res.status === OK
   });
 }
